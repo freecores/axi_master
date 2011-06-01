869a575462c739a61ad03a1ebf0b82dc5735c31c
eyalhoc <eyalhoc@5d80efc8-5b61-417b-a52e-b4bf991f7b44>
2011-06-01 11:32:05 +0000

    Support RobustVerilog project

--- run/run.sh
+++ run/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../../../robust -ver
+../../../robust -null
 if [ $? -eq 0 ];then
   ROBUST=../../../robust
 else
@@ -8,4 +8,5 @@ else
   ROBUST=../../../robust-lite
 fi
 
-$ROBUST ../src/base/axi_master.v -od out -I ../src/gen -list list.txt -listpath -header -gui ${@}
+#$ROBUST ../src/base/axi_master.v -od out -I ../src/gen -list list.txt -listpath -header -gui ${@}
+$ROBUST robust_axi_master.pro -gui ${@}
